23f7d86171a76708c8dbeabd525292293972a43e
SzymonSliwa <creedo@tlen.pl>
2023-02-21 21:45:37 +0100

    Update task-06.js
    Class remove, getAttribute

--- js/task-06.js
+++ js/task-06.js
@@ -1,13 +1,17 @@
 const textInput = document.getElementById("validation-input");
 
+const textInputLength = Number(textInput.getAttribute("data-length"));
+
 textInput.addEventListener("blur", (event) => {
   const text = event.currentTarget.value;
 
   const length = text.length;
 
-  if (length >= 6) {
+  if (length === textInputLength) {
     textInput.classList.add("valid");
+    textInput.classList.remove("invalid");
   } else {
     textInput.classList.add("invalid");
+    textInput.classList.remove("valid");
   }
 });
